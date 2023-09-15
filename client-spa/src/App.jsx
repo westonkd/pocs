4942ab07e75ad3f3c6dc0dddc52f3b984f943e14
Weston Dransfield <wdransfield@instructure.com>
2023-09-15 14:47:01 -0600

    Add initial client-spa

---
+++ client-spa/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+// From https://dev.to/franciscomendes10866/file-based-routing-using-vite-and-react-router-3fdo
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+
+const routes = [];
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/pages\/(.*)\.jsx$/)?.[1];
+  if (!fileName) {
+    continue;
+  }
+
+  const normalizedPathName = fileName.includes("$")
+    ? fileName.replace("$", ":")
+    : fileName.replace(/\/index/, "");
+
+  routes.push({
+    path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
+    Element: pages[path].default,
+    loader: pages[path]?.loader,
+    action: pages[path]?.action,
+    ErrorBoundary: pages[path]?.ErrorBoundary,
+  });
+}
+
+const router = createBrowserRouter(
+  routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+    ...rest,
+    element: <Element />,
+    ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+  }))
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
